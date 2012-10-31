a6b6b0ac56170d092ee0c63a2a526f23db51e9fc
Nikhil Mungel <mail@hyfather.com>
2012-10-30 23:32:25 -0700

    groups can look up all containing expenses

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ActiveRecord::Base
   
   has_many :memberships
   has_many :users, :through => :memberships
+
+  has_many :expenses
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Group do
   it { should have_many(:users).through(:memberships) }
+  it { should have_many(:expenses) }
 end
